d541c5b3b57c2a89c24559d0cf20d35859035664
Angelo Terzi <angeloterzi98@gmail.com>
2026-03-20 15:24:10 +0100

    :construction: Added a print to diagnose inner working of lowering pass

--- xdsl/backend/riscv/lowering/convert_arith_to_riscv_snitch.py
+++ xdsl/backend/riscv/lowering/convert_arith_to_riscv_snitch.py
@@ -34,7 +34,9 @@ class LowerBinaryFloatVectorOp(RewritePattern):
     def match_and_rewrite(self, op: Operation, rewriter: PatternRewriter) -> None:
         if not isinstance(op, self.arith_op_cls):
             return
-
+        print("OP _________________________________________")
+        print(op)
+        print("____________________________________________")
         operand_type = op.result.type
         if not isinstance(operand_type, VectorType):
             return
@@ -72,6 +74,53 @@ class LowerBinaryFloatVectorOp(RewritePattern):
 
         rewriter.replace_matched_op((lhs, rhs, new_op, cast_op))
 
+@dataclass
+class LowerEterogeneousBinaryFloatVectorOp(RewritePattern):
+    arith_op_cls: type[arith.FloatingPointLikeBinaryOp]
+    riscv_d_op_cls: type[riscv.RdRsRsFloatOperationWithFastMath]
+    riscv_snitch_v_f32_op_cls: type[riscv.RdRsRsFloatOperationWithFastMath]
+    riscv_snitch_v_f16_op_cls: type[riscv.RdRsRsFloatOperationWithFastMath]
+
+    def match_and_rewrite(self, op: Operation, rewriter: PatternRewriter) -> None:
+        if not isinstance(op, self.arith_op_cls):
+            return
+
+        operand_type = op.result.type
+        if not isinstance(operand_type, VectorType):
+            return
+        shape = operand_type.shape
+        count = prod(dim.data for dim in shape.data)
+
+        operand_type = cast(VectorType[Any], operand_type)
+        scalar_type = operand_type.element_type
+
+        lhs = UnrealizedConversionCastOp.get((op.lhs,), (_FLOAT_REGISTER_TYPE,))
+        rhs = UnrealizedConversionCastOp.get((op.rhs,), (_FLOAT_REGISTER_TYPE,))
+
+        match scalar_type:
+            case Float64Type():
+                if count != 1:
+                    return
+                cls = self.riscv_d_op_cls
+            case Float32Type():
+                if count != 2:
+                    return
+                cls = self.riscv_snitch_v_f32_op_cls
+            case Float16Type():
+                if count != 4:
+                    return
+                cls = self.riscv_snitch_v_f16_op_cls
+            case _:
+                assert False, f"Unexpected float type {op.lhs.type}"
+
+        rv_flags = riscv.FastMathFlagsAttr("none")
+        if op.fastmath is not None:
+            rv_flags = riscv.FastMathFlagsAttr(op.fastmath.data)
+
+        new_op = cls(lhs, rhs, rd=_FLOAT_REGISTER_TYPE, fastmath=rv_flags)
+        cast_op = UnrealizedConversionCastOp.get((new_op.rd,), (op.result.type,))
+
+        rewriter.replace_matched_op((lhs, rhs, new_op, cast_op))
 
 lower_arith_addf = LowerBinaryFloatVectorOp(
     arith.Addf, riscv.FAddDOp, riscv_snitch.VFAddSOp, riscv_snitch.VFAddHOp
